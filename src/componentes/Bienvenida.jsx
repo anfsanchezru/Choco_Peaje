e3490124953f32a137b4b6de7011b4716b9cbe15
Andrés <sandresf@uninorte.edu.co>
2021-12-06 23:25:54 -0500

    Componentes de botones y página usuario (incompleto)

--- src/componentes/Bienvenida.jsx
+++ src/componentes/Bienvenida.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 
 export default function Principal() {
---
+++ src/componentes/Usuarios.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import 'bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+
+export default function Principal() {
+
+    const barraBusqueda = {
+        position: 'absolute',
+        width: '869px',
+        height: '52px',
+        left: '219px',
+        top: '552px',
+
+        background: '#FDFDFD',
+        border: '1px solid #000000',
+        'box-sizing': 'border-box'
+    },
+
+    const titulosSeccion1 = {
+        position: 'absolute',
+        width: '422.62px',
+        height: '54.79px',
+        left: '127px',
+        top: '710px',
+
+        background: '#F05B71',
+        border: '1px solid #000000',
+        'box-sizing': 'border-box',
+        'box-shadow': '0px 4px 4px rgba(0, 0, 0, 0.25)'
+    }
+
+}
