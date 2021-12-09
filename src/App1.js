8fa67583127c6e3d06475c9242c0f12ef736da74
Andrés <sandresf@uninorte.edu.co>
2021-12-08 20:01:44 -0500

    Update App1

--- src/App1.js
+++ src/App1.js
@@ -2,19 +2,16 @@ import './App.css';
 import React, { useState } from "react";
 import { NavbarChoco } from './components/NavbarChoco';
 import BannerChoco from './components/BannerChoco';
-import Registro from './components/Registro';
-import User from './components/User';
-import Comofunciona from './components/Comofunciona';
+import Bienvenida from './components/Bienvenida';
+import Usuarios from './components/Usuarios';
+import Botones from './components/Botones';
 import {
   BrowserRouter,
   Route,
   Routes
 } from "react-router-dom";
-import Login from './components/Login';
-import Pagos from './components/Pagos';
-import { Container1 } from './components/styled/Container.styled';
-import Reportes from './components/Reportes';
-
+import Consorcios from './components/Consorcios';
+import Peajes from './components/Peajes';
 
 
 
@@ -30,15 +27,12 @@ function App() {
       <NavbarChoco logged={logged} setLogged={setLogged}/>
       
       <BannerChoco/>
-      <Container1/>
         <Routes>
-          <Route path="/" element={ <BannerChoco logged={logged}  setLogged={setLogged}/> }></Route>
-          <Route path="/login"  element={ <Login logged={logged}  setLogged={setLogged}/> } ></Route>
-          <Route path="/registro"  element={ <Registro/> } ></Route>
-          <Route path="/user"  element={ <User logged={logged}  setLogged={setLogged}/> } ></Route>
-          <Route path="/Comofunciona"  element={ <Comofunciona/> } ></Route>
-          <Route path="/pagos"  element={ <Pagos/> } ></Route>
-          <Route path="/reportes"  element={ <Reportes/> } ></Route>
+          <Route path="/" element={ <BannerChoco /> }></Route>
+          <Route path="/boton"  element={ <Botones /> } ></Route>
+          <Route path="/usuario"  element={ <Usuarios/> } ></Route>
+          <Route path="/consorcio"  element={ <Consorcios/> } ></Route>
+          <Route path="/peaje"  element={ <Peajes/> } ></Route>
         </Routes>
       
       
